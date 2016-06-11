4c04b7340a3d3eb3cd0bd9b1194f4d9338d8f3fa
unknown <>
2016-06-11 13:50:54 +0800

    Bitmap implementation done

--- greatest_gcd_sum_bitmap.py
+++ greatest_gcd_sum_bitmap.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# This function will be implemented as ROM to speed up the system
+# input: num, output: 64 bits row to represent its divisors
 def get_divisors(num):
     # the divisors of the num
     divisors = []
@@ -15,11 +17,17 @@ def get_divisors(num):
             bitrow[i] = 0
     return bitrow
 
+# Helper function, identical to priority encoder
+def get_leftmost(entry):
+    for i in xrange(64):
+        if entry[i] == 1:
+            return i
+
 input_data = [21, 8, 5, 21, 11, 12]
 
-# one bitmap to record the divisors of numbers
+# Table is a bitmap to record all divisors of the input numbers
 # row => divisor, col => index of number 
-# Table[i] records number which has divisor i
+# e.g. Table[i] records the numbers which have divisor i
 table = np.zeros((64, 64))
 cnts = np.zeros(64)
 
@@ -32,3 +40,21 @@ for index, num in enumerate(input_data):
             cnts[i] += 1
 
 # Phase2: remove pairs
+for gcd in xrange(63, 0, -1):
+    while cnts[gcd] >= 2:
+        # remove A from table
+        A = get_leftmost(table[gcd])
+        rowA = get_divisors(input_data[A])
+        for i in xrange(64):
+            if rowA[i] == 1:
+                table[i, A] = 0
+                cnts[i] -= 1
+        # remove B frome table
+        B = get_leftmost(table[gcd])
+        rowB = get_divisors(input_data[B])  
+        for i in xrange(64):
+            if rowB[i] == 1:
+                table[i, B] = 0
+                cnts[i] -= 1
+        # Output A, B, G.C.D
+        print A, B, gcd
